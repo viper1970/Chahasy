f24b25c639f45e7b304607c1e4e3caff046c8f91
seriousme <hans.klunder@xs4all.nl>
2015-06-14 17:32:21 +0200

    Initial commit

---
+++ public/js/objects.js
@@ -0,0 +1,35 @@
+{
+	'pages' : [{
+			'id' : 'p1',
+			'label' : 'Page 1',
+			'items' : ['i1', 'i2']
+		}, {
+			'id' : 'p2',
+			'label' : 'Page 2',
+			'items' : ['i3', 'i4']
+		}
+	],
+	'items' : [{
+			'id' : 'i1',
+			'label' : 'Item 1',
+			'type' : 'toggle'
+			'topic' : 'a/a'
+		}, {
+			'id' : 'i2',
+			'label' : 'Item 2',
+			'type' : 'output'
+			'topic' : 'a/b'
+		}, {
+			'id' : 'i3',
+			'label' : 'Item 3',
+			'type' : 'button'
+			'topic' : 'b/a'
+		}, {
+			'id' : 'i4',
+			'label' : 'Item 4',
+			'type' : 'input'
+			'topic' : 'b/b'
+		}
+	]
+
+}
