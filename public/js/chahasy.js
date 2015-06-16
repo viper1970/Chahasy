04e873ffc8bc961f05056477b98eba62af75c899
seriousme <hans.klunder@xs4all.nl>
2015-06-16 19:38:16 +0200

    Dynamically setting values from values.json

--- public/js/chahasy.js
+++ public/js/chahasy.js
@@ -1,28 +1,57 @@
 function setPage(url){
 	var newPage = pageIdx[url];
 	if (typeof(newPage) != 'undefined'){
-		// mark the old page as inactive
-		pages[pageIdx[currentPage]].active="";
-		currentPage = url;
-		// mark the new page as active
-		pages[newPage].active="active";
 		items=pages[newPage].items;
+		//console.log(JSON.stringify(items));
 		ractive.set({
 			pages:pages,
-			items:items
+			items:items,
+			currentPage: url
+		}).then ( function(){ 
+			$('.btn-toggle').click(function() {
+				var topic = $(this).data('topic');
+			
+				if (topicIdx[topic].value == "on"){
+					topicIdx[topic].value = "off";
+				}
+				else{
+					topicIdx[topic].value = "on";
+				}
+				console.log("Toggle event detected for", topicIdx[topic].label, " linked to topic ", topic, " now switching to ",topicIdx[topic].value);
+				ractive.update();
+			})
 		});
 	}
 }
 
+function setVal(topic,value){
+	if (topicIdx[topic] != 'undefined'){
+			console.log("Setting value for topic ", topic, " to ", value);
+			topicIdx[topic].value = value;
+			ractive.update();
+		}
+}
+	
+
+// load data via JSON, this should be replaced by listening to MQTT
+function loadData(){
+	$.getJSON('data/values.json').done(function (data) { 
+		for(var i = 0; i < data.values.length ; i++) {
+			var item = data.values[i];
+			setVal(item.topic, item.value);
+		}
+	});
+}
+
+
+
 function init(data){
 	var itemIdx={};
-	// Index items and fix the type so its easier to consume by ractive
+	// Index items 
 	for(var i = 0; i < data.items.length ; i++) {
-		var item=data.items[i];
-		var t=item.type;
-		item.type={};
-		item.type[t]=true;
+		var item = data.items[i];
 		itemIdx[item.id] = item;
+		topicIdx[item.topic] = item;
 	}
 	// Index pages by URL and replace items by links to items (if any)
 	
@@ -39,7 +68,6 @@ function init(data){
 	// lets see what we got
 	//console.log(JSON.stringify(pages));
 	//console.log(JSON.stringify(pageIdx));
-	//console.log(JSON.stringify(items));
 	//console.log(JSON.stringify(itemIdx));
 
 	// start at page 0
@@ -52,15 +80,21 @@ function init(data){
 	setPage(currentPage);
 	// listen for URL changes
 	window.onhashchange = function(){ setPage(location.hash)};
+	// start loading data, this should be replaced by listening to MQTT
+	loadData();
 }	
 
-var currentPage, pages=[], items={}, pageIdx={};
+var currentPage, pages=[], items={}, pageIdx={}, topicIdx={}; 
 
 // create the ractive object
 var ractive = new Ractive({
 	el: renderOutput,
-	template: '#renderTemplate'
+	template: '#renderTemplate',
+	data: {
+		formatTemp: function(val){ if (val) { return val + '°' }}
+	}
 });
 
- $.getJSON('js/objects.json').done(function (data) { init(data)});
+// load setup via JSON, this should be replaced by listening to MQTT
+ $.getJSON('data/objects.json').done(function (data) { init(data)});
  
